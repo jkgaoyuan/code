5ba253655274fa9ad372318450d951e50f894d75
jkgaoyuan <jkgaoyuan@gmail.com>
2018-10-11 23:31:27 +0800

    ex

--- helloworld.py
+++ helloworld.py
@@ -1,19 +1,32 @@
-print('你好我是jkgao')
+# print('你好我是jkgao')
+# count = 0
+# while (count<9):
+#     print('The count is:', count)
+#     count = count + 1
+# print('END')
+# numbers = [12, 37, 65, 100, 77, 53] #奇数偶数
+# even = []
+# odd = []
+# while len(numbers) > 0 :
+#     number = numbers.pop()
+#     if(number % 2 == 0) :
+#         even.append(number)
+#         print('even=', even)
+#     else:
+#         odd.append(number)
+#         print('odd=', odd)
+#
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''# 字符
+s3 = r'Hello, "Bart"'
+s4 = 'i\'m "ok"'
+print(n, f, s1, s2, s3, s4)
 
-count = 0
-while (count<9):
-    print('The count is:', count)
-    count = count + 1
-print('END')
-numbers = [12, 37, 65, 100, 77, 53]
-even = []
-odd = []
-while len(numbers) > 0 :
-    number = numbers.pop()
-    if(number % 2 == 0) :
-        even.append(number)
-        print('even=', even)
-    else:
-        odd.append(number)
-        print('odd=', odd)
+a = 'ABC'
+b = a
+a = 'pi'    #赋值
 
+print(b)
+print(a)
