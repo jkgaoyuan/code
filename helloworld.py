d28853c0f2f6ee66dd935730c8ae81576f92d735
jkgaoyuan <jkgaoyuan@gmai.com>
2018-10-27 16:57:45 +0800

    ex

--- helloworld.py
+++ helloworld.py
@@ -161,3 +161,8 @@
 # for B in L :
 #     print('Hello,',B,'!')
 #     print("Hello, %s!" % B)
+# 循环是让计算机做重复任务的有效的方法。
+#
+# break语句可以在循环过程中直接退出循环，而continue语句可以提前结束本轮循环，并直接开始下一轮循环。这两个语句通常都必须配合if语句使用。
+#
+# 要特别注意，不要滥用break和continue语句。break和continue会造成代码执行逻辑分叉过多，容易出错。大多数循环并不需要用到break和continue语句，上面的两个例子，都可以通过改写循环条件或者修改循环逻辑，去掉break和continue语句。
