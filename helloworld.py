09b5f5f831ea3c03b69d29c23e5f536394d7948c
jkgaoyuan <jkgaoyuan@gmai.com>
2018-10-27 16:14:17 +0800

    ex

--- helloworld.py
+++ helloworld.py
@@ -86,11 +86,57 @@
 #
 #使用list和tuple 有序列表 :tumple 不可变 list 可变
 #请用索引取出下面list的指定元素： Apple:Python: Lisa
-L = [
-    ['Apple', 'Google', 'Microsoft'],
-    ['Java', 'Python', 'Ruby', 'PHP'],
-    ['Adam', 'Bart', 'Lisa']
-]
+# L = [
+#     ['Apple', 'Google', 'Microsoft'],
+#     ['Java', 'Python', 'Ruby', 'PHP'],
+#     ['Adam', 'Bart', 'Lisa']
+# ]
 # B = L[0]
 # print(B[0])
-print(L[0][0],L[1][1],L[-1][-1])
+# print(L[0][0],L[1][1],L[-1][-1])
+
+
+#重要的判断语句 当if的条件满足时就会跳过其他的elif 和 else 。
+# if的完整结构:        |
+# if <条件判断1>:      |
+#     <执行1>         |
+# elif <条件判断2>:
+#     <执行2>
+# elif <条件判断3>:
+#     <执行3>
+# else:
+#     <执行4>
+#############
+# age = 20
+# if age >= 6:
+#     print('teenager')
+# elif age >= 18:
+#     print('adult')
+# else:
+#     print('kid')
+
+# 18.10. 27.
+# eg
+# 小明身高1.75，体重80.5kg。请根据BMI公式（体重除以身高的平方）帮小明计算他的BMI指数，并根据BMI指数：
+# #
+# # 低于18.5：过轻
+# # 18.5-25：正常
+# # 25-28：过重
+# # 28-32：肥胖
+# # 高于32：严重肥胖
+# # 用if-elif判断并打印结果：
+H = 1.75
+W = 80.5
+#BMI = W/pow(H,2) #平方的写法
+BMI = W/(H*H)
+print('BMI=',BMI)
+if BMI<=18.5:
+    print('过轻')
+elif 18.5<BMI<=25:
+    print('正常')
+elif 25<BMI<28:
+    print('过重')
+elif 28<BMI<=32:
+    print('肥胖')
+else :
+    print('严重肥胖')
